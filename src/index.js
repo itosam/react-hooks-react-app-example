1dab69056060f66dda577eb3fa7278cdfb5d0a28
Osamu Ito <mr.samito@gmail.com>
2022-09-17 15:45:07 -0400

    commit tree a6a01e716a3552696d8926ad7f0801e7cf0d523b

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+//rendering the import App from "./components/App"  in the "root" ID of the html.
+
